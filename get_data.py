bfd04b687720b26bbe112b9e2fe84014c9b33c3b
sindre eskeland <sindre_e@hotmail.com>
2019-09-04 19:25:22 +0200

    implemented api-interface in python

--- get_data.py
+++ get_data.py
@@ -2,14 +2,34 @@ import requests
 
 client_id = '07a7c3bb-8669-4253-ac39-c6fb21908dcc'
 
+name = "oslo"
+endpoint = 'https://frost.met.no/sources/v0.jsonld'
+parameters = {
+    "name" : f'*{name}*',
+}
+r = requests.get(endpoint,parameters, auth=(client_id,''))
+d = r.json()
+
+sensor = d["data"][0]["id"]
+
+
+
+# burde kunne velge tidsperiode?
+
 endpoint = 'https://frost.met.no/observations/v0.jsonld'
 parameters = {
-    'sources': 'SN18700,SN90450',
-    'elements': 'mean(air_temperature P1D)',
-    'referencetime': '1990-04-01/2018-04-03',
+    'sources': sensor,
+    'elements': 'mean(air_temperature P1M)',
+    'referencetime': '1990-04-01/1991-04-03',
 }
 
 r = requests.get(endpoint, parameters, auth=(client_id,''))
-json = r.json()
-print(r)
-#print(json)
+data = r.json()["data"]
+
+for d in data:
+    print(f'{d["referenceTime"]} : {d["observations"][0]["value"]} {d["observations"][0]["unit"]} ' )
+
+
+
+
+
